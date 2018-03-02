6daeefde64fe51f4d187e84de2505233e9d2ccb9
rmeany <ryan.meany@mycit.ie>
2018-03-02 17:28:09 +0000

    Created Home section. Changed colors to match CIT color scheme.

--- app/src/main/java/com/example/sportsandsocieties/HomeFragment.java
+++ app/src/main/java/com/example/sportsandsocieties/HomeFragment.java
@@ -6,8 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 /**
  * Created by abcd on 29/01/2018.
@@ -23,19 +21,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_home, container, false);
-
-        String [] array = {"Welcome"};
-
-        ListView listView = myView.findViewById(R.id.homeLV);
-
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                array
-        );
-
-        listView.setAdapter(listViewAdapter);
-
         return myView;
     }
 
